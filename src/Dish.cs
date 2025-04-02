6bfe6ee653a2dfae8c7d676e102675af8a7e37cb
Agustin Pechi <AGUSTIN.PECHI@correo.ucu.edu.uy>
2025-04-02 20:56:42 -0300

    Agrego el arte del menu

--- src/Dish.cs
+++ src/Dish.cs
@@ -3,6 +3,7 @@ namespace Ucu.Poo.Restaurant;
 /// <summary>
 /// Representa un platillo individual en el menú del restaurante.
 /// </summary>
+
 public class Dish
 {
     
--- src/Menu.cs
+++ src/Menu.cs
@@ -8,5 +8,24 @@ using System.Collections;
 /// </summary>
 public class Menu
 {
-    private ArrayList dishes = new ArrayList();
+    private ArrayList dishes = new ArrayList(); //Hace una lista
+    public void AddDish(Dish dish)
+    {
+        dishes.Add(dish);
+    }
+    public void RemoveDish(Dish dish)
+    {
+        dishes.Remove(dish);
+    }
+    public Dish GetDishByName(string name)
+    {
+        foreach (Dish dish in dishes)
+        {
+            if (dish.name.Equals(name, StringComparison.OrdinalIgnoreCase))
+            {
+                return dish;
+            }
+        }
+        return null;
+    }
 }
