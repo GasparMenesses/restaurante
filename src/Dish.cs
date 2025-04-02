309aafd27e92cd221bb6bc05c5f5db6f7be05ab9
Joaquin de Dios <joaquin.dedios@correo.ucu.edu.uy>
2025-04-02 19:59:07 -0300

    agrego dish

--- src/Dish.cs
+++ src/Dish.cs
@@ -1,9 +1,44 @@
+using System.Diagnostics;
+
 namespace Ucu.Poo.Restaurant;
 
 /// <summary>
 /// Representa un platillo individual en el menú del restaurante.
 /// </summary>
+
 public class Dish
 {
-    
+    public string name; 
+    public double price;
+    public bool isVegetarian;
+
+    public Dish(string name, double price, bool isVegetarian)
+    {
+        this.name = name;
+        this.price = price;
+        this.isVegetarian = isVegetarian;
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public double Price
+    {
+        get { return price; }
+    }
+
+    public bool IsVegetarian
+    {
+        get { return isVegetarian; }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Dish dish= new Dish("Hamburguesa", 850, false);
+    }
 }
